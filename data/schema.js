4214ff699ea0d1029fefe8bc350908ed4655e33e
Armand <apaquino@gmail.com>
2016-01-27 11:37:38 -0800

    add description to common types

--- data/schema.js
+++ data/schema.js
@@ -16,6 +16,7 @@ import axios from 'axios';
 function usersFollowing() {
   return {
       type: new GraphQLList(RepoInfoType),
+      description: "Fields about the people you this person follows",
       resolve: (obj) => {
         const brackIndex = obj.following_url.indexOf("{"),
               url =  obj.following_url.slice(0, brackIndex);
@@ -30,6 +31,7 @@ function usersFollowing() {
 function followingUrl() {
   return {
     type: GraphQLString,
+    description: "URI to get data on the people this person follows",
     resolve: (obj) => {
       const brackIndex = obj.following_url.indexOf("{");
       return obj.following_url.slice(0, brackIndex);
